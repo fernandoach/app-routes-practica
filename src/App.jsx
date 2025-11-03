a71547f7490550979954acfda5b72062f5edd3a5
Fernando A. <fernandoach333@gmail.com>
2025-11-03 14:47:28 -0500

    mejorar comentarios TODO con descripciones más detalladas

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { HomePage } from "./components/pages/homePage"
 
 function App() {
   /* 
-    TODO: Grupo 1: Ruta raiz
-    TODO: Grupo 2: Ruta /estudiantes
-    TODO: Grupo 3: Ruta /profesores
-    TODO: Grupo 4: Ruta /cursos
+    TODO: Grupo 1: Ruta raiz -> Landing page tienda de cursos
+    TODO: Grupo 2: Ruta /estudiantes -> panel para gestionar estudiantes
+    TODO: Grupo 3: Ruta /profesores -> panel para gestionar profesores
+    TODO: Grupo 4: Ruta /cursos -> panel para gestionar cursos
   */
   return (
     <>
